a7b588c5450c285ab6c80f5e75002aa6b8d54a6a
RAI015 <i.scryed72342019@gmail.com>
2020-06-21 20:51:11 +0900

    リスト化及びメモの編集

---
+++ src/views/Edit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="editor">
+    <h1>
+      <textarea name="memo" v-model="memo.body"></textarea>
+      <button @click="save">保存</button>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'edit',
+  computed: {
+    memo: function() {
+      let id = this.$route.params["id"];
+      return this.$store.state.memos.find(memo => memo.id == id);
+    }
+  },
+  methods: {
+    save: function() {
+      this.$store.commit("update", {
+        id: this.memo.id,
+        body: this.memo.body
+      });
+      this.$router.push('/');
+    }
+  }
+}
+</script>
