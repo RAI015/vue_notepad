9b57744a0ff261df6a42141e16d1e7346c0598da
RAI015 <i.scryed72342019@gmail.com>
2020-06-21 18:43:19 +0900

    メモデータの保存

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    memos: [
+      {id: 1, body: 'サンプルのメモです'}
+    ]
+  },
+  mutations: {
+    save(state, memo) {
+      // 新しいIDを割り振る
+      var max = state.memos[state.memos.length - 1].id;
+      memo.id = max + 1;
+
+      state.memos.push(memo);
+    }
+  }
+})
